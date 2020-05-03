cd970ae1ff2add9d0120a5e4609c236578c11b47
MilovanTomasevic <tomas.ftn.e2@gmail.com>
2020-05-03 23:00:08 +0200

    home/index page

--- edu/ecenter/admin.py
+++ edu/ecenter/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Slider, Slide, Baner, Content, About, Training, Stories
 
-# Register your models here.
+class InLineSlide(admin.StackedInline):
+    model = Slide
+    extra = 1
+    max_num = 5
+
+class SliderAdmin(admin.ModelAdmin):
+    inlines = [InLineSlide]
+
+class InLineContent(admin.StackedInline):
+    model = Content
+    extra = 1
+    max_num = 4
+
+class BanerAdmin(admin.ModelAdmin):
+    inlines = [InLineContent]
+
+
+admin.site.register(Slider, SliderAdmin)
+admin.site.register(Baner, BanerAdmin)
+admin.site.register(About)
+admin.site.register(Training)
+admin.site.register(Stories)
--- edu/ecenter/views.py
+++ edu/ecenter/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Slider, Baner, About, Training, Stories
+from blog.models import Post
+from courses.models import Course
+from events.models import Event
+from users.models import UserProfile
+from django.contrib import messages
+from django.http import HttpResponse , HttpResponseRedirect
 
 def home(request):
-    return render(request, 'index.html')
+    try:
+        slider = Slider.objects.last()
+        baner = Baner.objects.last()
+        about = About.objects.last()
+        courses = Course.objects.order_by('course_date')[:3]
+        events = Event.objects.order_by('event_date')[:3]
+        training = Training.objects.last()
+        stories = Stories.objects.last()
+        posts = Post.objects.order_by('-date_posted')[:3]
+        profiles = UserProfile.objects.all()[:3]
+
+        template = 'ecenter/index.html'
+        context = {
+            'slider' : slider,
+            'baner' : baner,
+            'about' : about,
+            'courses' : courses,
+            'events' : events,
+            'training' : training,
+            'stories' : stories,
+            'posts' : posts,
+            'profiles' : profiles
+        }
+        return render(request, template, context)
+    except:
+        messages.add_message(request, messages.INFO, 'It is necessary to insert data from the admin page')
+        return HttpResponseRedirect('/admin')
 
 def teacher(request):
     return render(request, 'teacher.html')
