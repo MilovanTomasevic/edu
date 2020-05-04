4c86da27aac2163bf08c97c0b9318ef107165631
MilovanTomasevic <tomas.ftn.e2@gmail.com>
2020-05-05 00:49:01 +0200

    added courses for a teacher in UserProfile

--- edu/users/views.py
+++ edu/users/views.py
@@ -3,12 +3,19 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserUpdateForm, ProfileUpdateForm
 from .models import UserProfile
+from courses.models import Course
 from django.views.generic import  DetailView, ListView
 
 def profile(request):
-    # context = UserProfile.objects.get(user=request.user)
-    # return render(request, 'users/profile.html', context)
-    return render(request, 'users/profile.html')
+    courses = Course.objects.filter(teacher=request.user.userprofile)
+    if courses.exists():
+        context = {
+        'courses' : courses,
+        }
+    else:
+        context = None
+
+    return render(request, 'users/profile.html', context)
 
 @login_required
 def update_profile(request):
@@ -37,4 +44,12 @@ def update_profile(request):
 
 class ProfileDetailView(DetailView):
     model = UserProfile
-    template_name = 'users/profile_detail.html'  #
+    template_name = 'users/profile_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        courses = Course.objects.filter(teacher__pk=self.kwargs.get('pk'))
+        context.update({
+            'courses' : courses,
+        })
+        return context
