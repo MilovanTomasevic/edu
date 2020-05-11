f9fe25dfc314292fcae1066c648d62576fa167d7
MilovanTomasevic <tomas.ftn.e2@gmail.com>
2020-05-11 22:55:01 +0200

    users model: customized for STORE - fields

--- edu/users/models.py
+++ edu/users/models.py
@@ -20,6 +20,8 @@ class UserProfile(models.Model):
     interests = models.ManyToManyField('Interests')
     biography = HTMLField('Biography')
     role = models.ManyToManyField('Role') # models.ForeignKey('Role', related_name='roles')
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
+    one_click_purchasing = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username} - {self.name}'  
