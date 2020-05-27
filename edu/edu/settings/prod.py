c0dac10ad629a21321904e2126335b708398563f
MilovanTomasevic <tomas.ftn.e2@gmail.com>
2020-05-27 20:51:08 +0200

    pripare heroku

--- edu/edu/settings/prod.py
+++ edu/edu/settings/prod.py
@@ -1,7 +1,12 @@
-dev.pyfrom .base import *
+import os
+import django_heroku
+from .base import *
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
+ALLOWED_HOSTS = ['*']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
@@ -12,14 +17,54 @@ AUTH_PASSWORD_VALIDATORS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
+
+# Allauth configuration
+
+ACCOUNT_SIGNUP_FORM_CLASS = 'users.forms.SignupForm'
+ACCOUNT_AUTHENTICATION_METHOD = "username"
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "[EduCenter]"
+ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN = 60
+# ACCOUNT_FORMS={
+#     "add_email": "edu.forms.allauth.MyAddEmailForm",
+#     "change_password": "edu.forms.allauth.MyChangePasswordForm",
+#     "disconnect": "edu.forms.allauth.MyDisconnectForm",
+#     "login": "edu.forms.allauth.MyLoginForm",
+#     "reset_password": "edu.forms.allauth.MyResetPasswordForm",
+#     "reset_password_from_key": "edu.forms.allauth.MyResetPasswordKeyForm",
+#     "set_password": "edu.forms.allauth.MySetPasswordForm",
+#     "signup": "edu.forms.allauth.MySignUpForm"
+# }
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 10
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 60
+ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE = True
+ACCOUNT_LOGIN_ON_PASSWORD_RESET = False
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_BLACKLIST =["educenter","edu","admin"]
+ACCOUNT_USERNAME_MIN_LENGTH = 6
+# ACCOUNT_USERNAME_VALIDATORS =None
+
+django_heroku.settings(locals())
